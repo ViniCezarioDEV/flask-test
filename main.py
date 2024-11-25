f29e4c943a7671eba22308e477ca2ae1d4a46354
vinicezariodev <viniciusamcezario@gmail.com>
2024-11-25 17:39:40 -0300

    adding buttom and input

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+        return f"{user_input}"
+
     return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
